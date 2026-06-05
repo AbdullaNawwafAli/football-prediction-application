e93d094544af1da40541886f385f82bf0ac47edb
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2026-06-05 16:13:59 +0500

    feat: change db to use local db instead of live db

--- src/lib/supabase/supabase.ts
+++ src/lib/supabase/supabase.ts
@@ -1,7 +1,14 @@
 import { createClient } from '@supabase/supabase-js'
 import type { Database } from '@/types/database.types'
 
+// for local development
 export const supabase = createClient<Database>(
-    import.meta.env.VITE_SUPABASE_URL,
-    import.meta.env.VITE_SUPABASE_ANON_KEY
+    import.meta.env.NEXT_PUBLIC_SUPABASE_URL,
+    import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
+
+// TODO: change to this for production
+// export const supabase = createClient<Database>(
+//     import.meta.env.VITE_SUPABASE_URL,
+//     import.meta.env.VITE_SUPABASE_ANON_KEY
+// )
