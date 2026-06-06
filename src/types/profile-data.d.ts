9b26cf1c62762f241135397d6152b646d972eb58
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2026-06-06 16:27:37 +0500

    feat: promote profile data types from onboarding feature

--- src/types/profile-data.d.ts
+++ src/types/profile-data.d.ts
@@ -1,9 +1,10 @@
 import type { Database } from "#/types/database.types"
 
 export type ProfileInsert = Database["public"]["Tables"]["profiles"]["Insert"]
-export type ProfileRow = Database["public"]["Tables"]["profiles"]["Row"]
 
-export interface SubmitProfileParams {
+export type ProfileData = Database["public"]["Tables"]["profiles"]["Row"]
+
+export interface CreateProfileDto {
   userId: string
   displayName: string
   profilePicture: File | undefined
