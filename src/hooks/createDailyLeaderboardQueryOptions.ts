e45dd9dc59e7542394447d2cc0e2cf8b05dbddc9
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2026-06-14 11:42:17 +0500

    feat; delete unused component

--- src/hooks/createDailyLeaderboardQueryOptions.ts
+++
@@ -1,9 +0,0 @@
-import { queryOptions } from '@tanstack/react-query'
-import { getDailyLeaderboardApi } from '#/services/getDailyLeaderboard'
-
-export default function createDailyLeaderboardQueryOptions(date: Date) {
-  return queryOptions({
-    queryKey: ['leaderboard', 'daily', date.toDateString()],
-    queryFn: () => getDailyLeaderboardApi(date),
-  })
-}
